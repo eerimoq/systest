eef77fad74b3c0fcfa553902410fadcad228edbb
erik <erik.moqvist@gmail.com>
2016-02-02 19:05:47 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='systest',
-      version='0.1.0',
+      version='0.2.0',
       description=('System test framework.'),
       long_description=open('README.rst', 'r').read(),
       author='Erik Moqvist',
