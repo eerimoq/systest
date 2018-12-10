a9da55ad00113aaac5a918b04f1dd4be48fe6ae2
Erik Moqvist <erik.moqvist@gmail.com>
2018-12-10 08:57:26 +0100

    Showcase all asserts in basics example.

--- examples/basics.py
+++ examples/basics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# A simple example showcasing the basics.
+# A simple example showcasing the basics of systest.
 #
 
 import logging
@@ -23,9 +23,17 @@ class MyTestCase(systest.TestCase):
         LOGGER.info("Hello!")
 
         self.assert_equal(1, 1)
+        self.assert_not_equal(1, 2)
         self.assert_true(1 == 1)
+        self.assert_false(1 == 2)
         self.assert_in(1, [1, 2])
-        self.assert_none(None)
+        self.assert_not_in(1, [0, 2])
+        self.assert_is_none(None)
+        self.assert_is_not_none(1)
+        self.assert_greater(2, 1)
+        self.assert_greater_equal(2, 2)
+        self.assert_less(1, 2)
+        self.assert_less_equal(2, 2)
 
         with self.assert_raises(RuntimeError) as cm:
             raise RuntimeError('foo')
